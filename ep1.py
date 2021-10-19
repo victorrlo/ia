26ab557c30c64de20260973f4475ba4dbfe32d66
Victor Leta <victor.rl.oliveira@gmail.com>
2021-10-19 01:21:16 -0300

    Initializing environment
    Randomized dust to every cell of the environment.

--- ep1.py
+++ ep1.py
@@ -1,1 +1,27 @@
 #Exercício Prático 1 - Simulador e Agente Reativo Simples em Ambiente Parcialmente Observável
+import random
+import numpy as np
+
+def environment(row_size, column_size):
+     #sujeira na célula do ambiente = 1
+     #limpo = 0
+
+    x = []
+    y = []
+
+    #initiate cells
+    #randomize dust
+    deck = list(range(0,100))
+    cells = np.full((row_size, column_size), 0)
+    for i in range(row_size):
+        for j in range(column_size):
+            random.shuffle(deck)
+            c = deck.pop()
+            if(c >= 50):
+                cells[i][j] = 1
+            elif(c < 50):
+                cells[i][j] = 0
+                
+    return cells
+
+print(environment(1, 2))
