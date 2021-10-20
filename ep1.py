13bbc55a61822f84cd4c2d3f1bc7858dc46cf8f6
Victor Leta <victor.rl.oliveira@gmail.com>
2021-10-20 14:24:47 -0300

    Update ep1.py
    Finalização do projeto de Agente Reativo Simples para um ambiente 2D com duas salas.
    Movimentação apenas para direita ou esquerda e limpar ou noop(nenhuma operação).

--- ep1.py
+++ ep1.py
@@ -1,27 +1,110 @@
 #Exercício Prático 1 - Simulador e Agente Reativo Simples em Ambiente Parcialmente Observável
 import random
 import numpy as np
+import time
 
-def environment(row_size, column_size):
-     #sujeira na célula do ambiente = 1
-     #limpo = 0
+def reactive_agent(row_size, column_size, cells, elapsed_time, score):
+    
+    #posição do agente inicial desconhecida:
+    ini_row = 0#random.randint(0,row_size-1) 
+    ini_col = 1#random.randint(0,column_size-1)
 
-    x = []
-    y = []
+    #sensor do agente
+    reactive_agent.sensor = cells[ini_row][ini_col]
 
-    #initiate cells
-    #randomize dust
-    deck = list(range(0,100))
-    cells = np.full((row_size, column_size), 0)
-    for i in range(row_size):
-        for j in range(column_size):
-            random.shuffle(deck)
-            c = deck.pop()
-            if(c >= 50):
-                cells[i][j] = 1
-            elif(c < 50):
-                cells[i][j] = 0
-                
-    return cells
+    if(row_size == 1):
+    
+        for i in range(0,elapsed_time):
 
-print(environment(1, 2))
+            #aleatoriedade de sujeira
+            random_dust = random.randint(0,1)
+            if random_dust == 1:
+                cells[random.randint(0,row_size-1)][random.randint(0,column_size-1)] = 1
+
+            #estado inicial
+            print("Ambiente")
+            print(cells)
+            
+            #operação de limpeza
+            if(reactive_agent.sensor == 0):
+                print("Célula limpa.")
+                print("Procurando nova célula para limpar...")
+                #time.sleep(5)
+                print(cells)
+
+            elif(reactive_agent.sensor == 1):
+                print("Célula suja.")
+                print("Limpando Célula do Ambiente...")
+                #time.sleep(5)
+                cells[ini_row][ini_col] = 0
+                print("Limpeza finalizada!")
+                score = score +1
+                print(cells)
+
+            else:
+                print("ERRO! Célula com dados desconhecidos!")
+                print(cells)
+
+            #operação de movimento
+            right = 0
+            left = 0
+        
+            if((ini_col == 0 or ini_col+1 < column_size-1) and left == 0): #existe espaço para a direita
+                if(ini_col < column_size-1):
+                    right = 1
+                    #movimento para a direita
+                    ini_col = ini_col+1
+                    reactive_agent.sensor = cells[ini_row][ini_col]
+                else:
+                    right = 0
+                    print("posicao: ", ini_row, ini_col)
+                    print("Agente no limite direito do ambiente")
+            
+            
+            elif((ini_col == column_size-1) and right == 0): #não existe espaço para a direita
+                left = 1
+                if(ini_col > 0):
+                    left = 1
+                    #movimento para a esquerda
+                    ini_col = ini_col-1
+                    reactive_agent.sensor = cells[ini_row][ini_col]
+                else:
+                    left = 0
+                    print("posicao: ", ini_row, ini_col)
+                    print("Agente no limite esquerdo do ambiente")
+
+            else: #saiu do ambiente
+                print("ERRO! Posição impossível!")
+
+                if(ini_col > column_size-1):
+                        ini_col = ini_col-1
+
+                elif(ini_col < 0):
+                    ini_col = ini_col+1
+        print("-----------------------------------------------------------")
+    else:
+        print("ERRO! Ambiente 3D! Projeto conta apenas com ambiente 2D!")
+        
+    return score
+    
+                             
+
+#dimensões do ambiente
+row_size = 1
+column_size = 2 
+
+#pontuação
+score = 0
+
+#tempo de simulação
+elapsed_time = 1000
+
+#células do ambiente inicializadas
+cells = np.full((row_size, column_size), 0)
+
+cells[0][0] = 1
+cells[0][1] = 0
+
+#simulação
+score = reactive_agent(row_size, column_size, cells, elapsed_time, score)
+print(score)
